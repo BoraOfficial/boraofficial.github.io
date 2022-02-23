e7a39e987e77aa56173ff2196ac7f4c365d32a64
BoraOfficial <94760052+BoraOfficial@users.noreply.github.com>
2022-02-23 16:48:08 +0300

    Add files via upload

---
+++ Projects/OnlineExam/script.js
@@ -0,0 +1,31 @@
+// One-time use only
+
+if (localStorage.getItem("visited")) {
+    window.location.href = "./Access Denied/index.html";
+}
+localStorage.setItem("visited", "true");
+
+// When user tabs out
+document.documentElement.addEventListener('mouseleave', () => location.reload())
+
+
+
+// User can't view the source
+
+document.onkeydown = function(e) {
+  if(event.keyCode == 123) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+     return false;
+  }
+}
