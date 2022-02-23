e2467795d42aa0d37126421044672343a7562180
BoraOfficial <94760052+BoraOfficial@users.noreply.github.com>
2022-02-23 17:12:54 +0300

    Update script.js

--- Projects/OnlineExam/Access Denied/script.js
+++ Projects/OnlineExam/Access Denied/script.js
@@ -1,7 +1,5 @@
 // User can't view the source
 
-<span id="rcp-span" style="display: none; position: fixed; overflow: hidden;"></span>
-<script id="rcp-script" type="text/javascript">var rcpSpan = document.getElementById('rcp-span');window.onmousemove = function (e) { var x = e.clientX, y = e.clientY; rcpSpan.style.top = (y + 20) + 'px'; rcpSpan.style.left = (x + 20) + 'px'; };window.ondragstart = function (e) { if (e && e.target && (e.target.nodeName == 'IMG' || e.target.nodeName == 'img')) { return false;} };if (document.addEventListener) { document.addEventListener('contextmenu', function(e) { showCR(); e.preventDefault(); }, false); } else { document.attachEvent('oncontextmenu', function() { showCR(); window.event.returnValue = false; }); }function showCR() { rcpSpan.style.display = 'block'; window.setTimeout(closeCR,2000); }function closeCR() { rcpSpan.style.display = 'none'; }</script>
 
 document.onkeydown = function(e) {
   if(event.keyCode == 123) {
