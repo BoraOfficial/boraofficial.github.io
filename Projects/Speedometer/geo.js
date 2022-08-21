3d4f9bd4b65ecd03b32851977f625c5b4dab1d36
BoraOfficial <94760052+BoraOfficial@users.noreply.github.com>
2022-08-21 23:32:09 +0300

    Update geo.js

--- Projects/Speedometer/geo.js
+++ Projects/Speedometer/geo.js
@@ -1,18 +1,46 @@
-setInterval(function(){
-  var velo = document.getElementById("velo");
+function toRad(degrees) {
+  return degrees * Math.PI / 180;
+};    
 
-  function getlocation() {
-    navigator.geolocation.getCurrentPosition(showLoc);
-  }
+function calcCrow(lat1, lon1, lat2, lon2) {
+  var R = 6371000; // meter
+  var dLat = toRad(lat2-lat1);
+  var dLon = toRad(lon2-lon1);
+  var lat1 = toRad(lat1);
+  var lat2 = toRad(lat2);
 
-  function showLoc(pos) {
-    velo.value = pos.coords.speed;
+  var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c;
+  return d;
+}
+
+var prevPosition = null;
+
+function showPosition(position) {
+  var lat2 = position.coords.latitude;
+  var lon2 = position.coords.longitude;
+  
+  if (prevPosition) {     
+    var lat1 = prevPosition.coords.latitude;
+    var lon1 = prevPosition.coords.longitude;
+    var distance = calcCrow(lat1,lon1,lat2,lon2).toFixed(1);
+    document.getElementById("velo").value = distance;
   }
-}, 1000);
+    
+
+  
+  prevPosition = position;
+  
+  setTimeout(function() {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  }, 1000);
+}
 
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
+} else {
   document.getElementById("div").innerHTML =
   "Geolocation is not supported by this browser.";
 }
